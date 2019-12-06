a04f7c5c3d870348d1ac15580e20266230a99692
Jared Obozele <jared.obozele@gmail.com>
2019-12-06 17:41:40 -0500

    new files added

---
+++ src/main/java/com/example/demo203/Demo203Application.java
@@ -0,0 +1,13 @@
+package com.example.demo203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo203Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo203Application.class, args);
+	}
+
+}
